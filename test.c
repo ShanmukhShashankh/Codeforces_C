2b5a51056839c96f510de44a1f0dee791e5eb89b
ShanmukhShashankh <70257931+ShanmukhShashankh@users.noreply.github.com>
2022-01-25 23:16:06 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+int unique_char(char* str) {
+	int hash[128] = { 0 };
+	int i, c = 0;
+	for (i = 0; i < strlen(str); ++i) {
+		hash[str[i]] = 1;
+	}
+	for (i = 0; i < 128; ++i) {
+		c += hash[i];
+	}
+	return c;
+
+}
+
+
+int main()
+{
+    char X[100];
+    scanf("%s",&X);
+    if(unique_char(X)%2==0){
+        printf("CHAT WITH HER!");
+        }
+    else{
+        printf("IGNORE HIM!");
+    }
+}         
